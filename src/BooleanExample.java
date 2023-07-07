db8db2b69e3ee8e7774f172635a0ba7fc73c7f57
kyj9515 <ydh981@naver.com>
2023-07-07 17:39:16 +0900

    first commit

---
+++ src/BooleanExample.java
@@ -0,0 +1,20 @@
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		boolean tf1 = true;
+		boolean tf2 = false;
+		boolean tf3 = 10 > 5;	//true
+		boolean tf4 = 10.5 < 0; //false
+//		boolean tf5 = 1;
+		
+		System.out.println("tf1=" + tf1);
+		System.out.println("tf2=" + tf2);
+		System.out.println("tf3=" + tf3);
+		System.out.println("tf4=" + tf4);
+		
+		
+
+	}
+
+}
---
+++ src/IntegerExample.java
@@ -0,0 +1,19 @@
+
+public class IntegerExample {
+
+	public static void main(String[] args) {
+		int max = 0b01111111_11111111_11111111_11111111;
+		int min = 0b10000000_00000000_00000000_00000000;
+		
+		
+		System.out.println("max=" + max);
+		System.out.println("min=" + min);
+		
+		max = max + 1;
+		System.out.println("max=" + max);
+		
+		min = min - 1;
+		System.out.println("min=" + min);
+	}
+
+}
