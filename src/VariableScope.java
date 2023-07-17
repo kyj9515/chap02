d6dc6c75c0a6653a4e038a63c86a6c42bd2e6397
kyj9515 <ydh981@naver.com>
2023-07-17 18:06:10 +0900

    ...

--- src/VariableScope.java
+++ src/VariableScope.java
@@ -3,14 +3,14 @@ public class VariableScope {
 
 	public static void main(String[] args) {
 		
-		System.out.println(true + null);
-//		int num = 100;
-//		
-//		{
-//			int num2 = 200;
-//		System.out.println(num);
-//		System.out.println(num2);
-//		}
+		
+		int num = 100;
+		
+		{
+			int num2 = 200;
+		System.out.println(num);
+		System.out.println(num2);
+		}
 	}
 	
 //	System.out.println(num);
